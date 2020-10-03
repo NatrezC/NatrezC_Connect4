d8a603aa9eb8786a842ff34ce709893bd14c83bf
NatrezC <cnatrez@gmail.com>
2020-10-03 02:12:24 -0500

    created the game board

--- app.js
+++ app.js
@@ -22,11 +22,22 @@ const tableColumn = document.getElementsByTagName('td')//grabs each spot within 
 let mySpot = document.querySelectorAll('tr')
 const player1 = "red"
 const player2 = "yellow"
+let noPlay = "white"
 const resetButton = document.querySelector('.reset')
 let redTurn = true 
 let turn = document.querySelector('.turn')
+const rows = ["F", "E", "D", "C", "B", "A"]
+//let Connect4 = {}
 
-
+let Connect4 =[
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0]
+]
+const numberToWin = 4;
 
 
 //Functions/////////////////
@@ -39,7 +50,7 @@ let turn = document.querySelector('.turn')
 // }
 function togglePlayers() {
     const player = document.getElementById('turn');
-    console.log(player)
+    //console.log(player)
     if (player.innerHTML === "Player 1's Turn"){
         player.innerHTML = "Player 2's Turn"
         document.getElementById('turn').style.color = "yellow"
@@ -48,16 +59,24 @@ function togglePlayers() {
         document.getElementById('turn').style.color = "red"
     }
 }
+function playColor(placement, currentPlayer, slot, row) {
+    // console.log(tableColumn,'????')
+    // // let column = event.target.cellIndex;
+    // // let row = [];
+    // for (let i = 5; i > -1; i--) {
+    //     if (tableColumn[i] === 5) {
+    //         slot.classList.add(red)
+    //     }
+    //     placement.classList.add(currentPlayer)
+    // }
+    // console.log(row, "//////")
+    // for (i = 5; i > -1; i--){
+    //     if (tableRow[5] === noPlay) {
+    //         placement.classList.add(currentPlayer)
+    //     }
+        //console.log(rows[i])
 
-function playColor(placement, currentPlayer,slot) {
-    let column = event.target.cellIndex;
-    let row = [];
-    for (let i = 5; i > -1; i--) {
-        if (tableColumn[i] === 5) {
-            slot.classList.add(red)
-        }
-        console.log(slot)
-    }
+    //}
 }
 
 function switchTurns() {
@@ -80,13 +99,14 @@ function handleClick(event) {
     //console.log(row)
     //let column = event.target.cellIndex
     let slot = (`${event.target.parentElement.rowIndex}, ${event.target.cellIndex}`)
+    console.log(slot)
     let placement = event.target
     //console.log(placement)
     //console.log(slot)
-    //console.log(row)
+    //console.log(row, "........")
     //console.log(column)
     const currentPlayer = redTurn ? player1 : player2
-    playColor(placement, currentPlayer, slot)
+    playColor(placement, currentPlayer, slot, row)
     togglePlayers()
     switchTurns()
 };
@@ -96,6 +116,29 @@ for (let i = 0; i < tableColumn.length; i++){
     tableColumn[i].addEventListener('click', handleClick)
 
 }
+
+//vars we have:
+//e.target -> clicked slot
+//slot -> string: row, col
+
+// loop through the slots in the column of the clicked slot, starting at row 0
+// ex:
+// if clicked slot is B3 (row=2, col=3),
+// check 6, 3
+// check 5, 3
+// check 4, 3, etc.
+// when checking, IF the slot you're currently checking is full (has class of red or yellow), then check the next available slot, OTHERWISE add class of red or yellow
+// <==>
+// a loop through the cells from column of selected slot, from bottom to top {
+//     if(curr.classList.contains('red')||curr.clasList.contains('yellow')) {
+//         move on
+//     } else {
+//         add the appropriate class
+//         break out of the loop (so that it doesn't keep adding classes to available cells)
+//     }
+// }
+
+
 
 
 
